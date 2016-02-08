8a2cb7cb15e1f01360c8c48510700383b6ba3bd4
Talento90 <marcotalento90@gmail.com>
2016-02-08 20:19:20 +0000

    fix model schema

--- src/models/image.js
+++ src/models/image.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose'
 
 var Schema = mongoose.Schema
 
-var TodoSchema = new Schema({
+var ImageSchema = new Schema({
   hash: { type: String, required: true, index: true },
   contentType: { type: String, required: true },
   url: { type: String, required: true },
   createdDate: { type: Date, default: Date.now }
 })
 
-export default mongoose.model('Image', TodoSchema)
+export default mongoose.model('Image', ImageSchema)
